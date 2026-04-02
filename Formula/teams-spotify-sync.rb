50605b5b71d5d9ad8f99c7e70f31d5b4ff46328a
Solomon <solomon.rubin@lacework.net>
2026-04-02 12:39:28 -0400

    Update teams-spotify-sync formula to v1.1.1

--- Formula/teams-spotify-sync.rb
+++ Formula/teams-spotify-sync.rb
@@ -1,8 +1,8 @@
 class TeamsSpotifySync < Formula
   desc "Auto-adjust Spotify volume during Microsoft Teams meetings"
   homepage "https://github.com/serubin/teams-spotify-sync"
-  url "https://github.com/serubin/teams-spotify-sync/archive/refs/tags/v1.0.0.tar.gz"
-  sha256 "eb3719dbe004118d0c2e32c2b5ee5f78d8052b41669c0818ea6bfc9cfc063d8c"
+  url "https://github.com/serubin/teams-spotify-sync/archive/refs/tags/v1.1.1.tar.gz"
+  sha256 "628888b2eeafba2f1bfc4d184433d4f9ae64195f381ea095e7e926d4ed53e77c"
   license "MIT"
 
   depends_on :macos
